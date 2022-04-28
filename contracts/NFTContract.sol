e2d5c882978a7c73568704d00949ab0dc6fccd1a
Geordan Sanchez <geordan.sanchez.contractor@bbva.com>
2022-04-27 21:28:36 -0500

    deploy to rinkeby

--- contracts/NFTContract.sol
+++ contracts/NFTContract.sol
@@ -1,10 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.1;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract NFTContract {
-    constructor() {
+contract NFTContract is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("SquareNFT", "SQUARE") {
         console.log("This is my NFT Contract");
+    }
+
+    function makeAnNFT() public {
+        uint256 newItemId = _tokenIds.current();
+
+        _safeMint(msg.sender, newItemId);
+
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/6AX0");
+        console.log(
+            "An NFT w/ ID %s has been minted to %s",
+            newItemId,
+            msg.sender
+        );
+
+        _tokenIds.increment();
     }
 }
