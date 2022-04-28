65664f1340c6847fd1523bcd5d0b66bd3e4f9974
Geordan Sanchez <geordan.sanchez.contractor@bbva.com>
2022-04-27 20:01:05 -0500

    First build

---
+++ contracts/NFTContract.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.1;
+
+import "hardhat/console.sol";
+
+contract NFTContract {
+    constructor() {
+        console.log("This is my NFT Contract");
+    }
+}
